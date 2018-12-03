99ab0cd3fc7eb9edf8457792eed5871487b807c4
bfv <dev@bfv.io>
2018-12-03 17:34:18 +0100

    day2 optimized

--- day2/day2b.ts
+++ day2/day2b.ts
@@ -1,13 +1,9 @@
 
 import { Input } from './input';
 
-let found2s = 0;
-let found3s = 0;
-
 function checkStrings (str1: string, str2: string) {
 
     let diffs: number[] = [];
-    let found3 = false;
     let result = '';
 
     for (let i = 0; i < str1.length; i++) {
@@ -18,27 +14,16 @@ function checkStrings (str1: string, str2: string) {
 
     if (diffs.length == 1) {
         let pos = diffs[0];
-        if (pos > 0) {
-            result = str1.substring(0, pos) + str1.substring(++pos);
-        }
-        else {
-            result = str1.substring(1);
-        }
+        result = str1.substring(0, pos) + str1.substring(++pos);
     }
 
     return result;
 }
 
 let result = '';
-for (let i = 0; i < Input.data.length; i++) {
-    for (let j = 0; j < Input.data.length; j++) {
+for (let i = 0; i < Input.data.length && result == ''; i++) {
+    for (let j = 0; j < Input.data.length && result == ''; j++) {
         result = checkStrings(Input.data[i], Input.data[j]);
-        if (result != '') {
-            break;
-        }
-    }
-    if (result != '') {
-        break;
     }
 }
 
