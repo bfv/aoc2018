ce78f8ebd1ab829989750a2f368ecc1aa7442e6b
bfv <dev@bfv.io>
2018-12-04 17:41:50 +0100

    optimized version (~100x faster)

--- day1/day1b.ts
+++ day1/day1b.ts
@@ -4,17 +4,17 @@ let t1 = new Date().getTime();
 
 let current = 0;
 let result: number;
-let freqs: number[] = [];
+let freqs = {};
 let iteration = 0;
 
 do {
     for (let i = 0; i < Input.data.length; i++) {
         current += Input.data[i];
-        if (freqs.indexOf(current) > -1) {
+        if (inThere(current)) {
             result = current;
             break;
         }
-        freqs.push(current);
+        store(current);
     }
     iteration++;
 } while (result === undefined);
@@ -24,3 +24,11 @@ console.log('found in iteration:', iteration);
 
 let t2 = new Date().getTime();
 console.log('time:', (t2 - t1), 'ms');
+
+function inThere(num: number) {
+    return (freqs['a' + num.toString()] !== undefined);
+}
+
+function store(num: number) {
+    freqs['a' + num.toString()] = true;
+}
